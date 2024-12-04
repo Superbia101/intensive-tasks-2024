d73f7dba16c50c4d624cf43be89970251248abef
Superbia101 <cherkmark@yandex.ru>
2024-12-04 14:11:42 +0800

    14

--- src/main/java/com/walking/intensive/chapter3/task14/Task14.java
+++ src/main/java/com/walking/intensive/chapter3/task14/Task14.java
@@ -32,7 +32,7 @@ package com.walking.intensive.chapter3.task14;
  * <ul>
  * <li>objectCounts[0] = 3, потому что радар с координатами (2;3) и радиусом действия 1 видит объекты с координатами
  * (1;3), (2;2) и (3;3). Всего 3 объекта.
- *</ul>
+ * </ul>
  *
  * <p>При наличии некорректных входных данных верните из метода пустой массив.
  *
@@ -44,10 +44,51 @@ package com.walking.intensive.chapter3.task14;
 public class Task14 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
+//        System.out.println(Arrays.toString(getObjectCounts(new int[][]{{1, 3}, {3, 3}, {5, 3}, {2, 2}},
+//                new int[][]{{2, 3, 1}, {4, 3, 1}, {1, 1, 2}})));
     }
 
     static int[] getObjectCounts(int[][] objectLocations, int[][] radars) {
-        // Ваш код
-        return new int[0];
+        if (!isValidation(objectLocations, radars)) {
+            return new int[0];
+        }
+
+        int countRadars = radars.length;
+        int[] objectCounts = new int[countRadars];
+
+        for (int i = 0; i < countRadars; i++) {
+            for (int[] j : objectLocations) {
+                if (Math.pow(j[0] - radars[i][0], 2) + Math.pow(j[1] - radars[i][1], 2) <= Math.pow(radars[i][2], 2)) {
+                    objectCounts[i] += 1;
+                }
+            }
+        }
+
+        return objectCounts;
+    }
+
+
+    static boolean isValidation(int[][] objectLocations, int[][] radars) {
+        if (objectLocations.length == 0 || radars.length == 0) {
+            return false;
+        }
+
+        boolean validationFlag = true;
+
+        for (int[] i : objectLocations) {
+            if (i.length != 2) {
+                validationFlag = false;
+                break;
+            }
+        }
+
+        for (int[] i : radars) {
+            if (i.length != 3 || i[2] < 0) {
+                validationFlag = false;
+                break;
+            }
+        }
+
+        return validationFlag;
     }
 }
