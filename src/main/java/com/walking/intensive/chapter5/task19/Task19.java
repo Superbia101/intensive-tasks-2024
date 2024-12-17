99e3803b8b7aefe0591a516c4e00dab01f0e93d2
Superbia101 <cherkmark@yandex.ru>
2024-12-17 09:46:01 +0800

    19, не закончено

--- src/main/java/com/walking/intensive/chapter5/task19/Task19.java
+++ src/main/java/com/walking/intensive/chapter5/task19/Task19.java
@@ -18,6 +18,14 @@ package com.walking.intensive.chapter5.task19;
 public class Task19 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
+        Sphere sphereNum1 = new Sphere(0, 0.5, 0, 8);
+        Sphere sphereNum2 = new Sphere(10, 2, 0, 3);
+        System.out.println(sphereNum2.getX());
+        System.out.println(Sphere.isIntersectSphere(sphereNum1, sphereNum2));
+        Parallelepiped objectNum1 = new Parallelepiped(0, 0, 0, 1, 1, 1);
+//        Parallelepiped objectNum2 = new Parallelepiped(0.5, 0.5, 0.5, -3, -3, -3);
+        Parallelepiped objectNum2 = new Parallelepiped(-1, -1, -1, -3, -3, -3);
+        System.out.println(Parallelepiped.isIntersectParallelepiped(objectNum1, objectNum2));
     }
 
     static boolean isIntersected(Sphere sphere, Parallelepiped parallelepiped) {
